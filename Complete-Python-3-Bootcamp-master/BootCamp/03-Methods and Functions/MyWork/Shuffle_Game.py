897a1084d5d1edd72ce40756443624d20ecddd3e
Deven Suji <Deven.Suji.ap@nielsen.com>
2021-01-05 06:23:21 +0530

    I'm trying

---
+++ Complete-Python-3-Bootcamp-master/BootCamp/03-Methods and Functions/MyWork/Shuffle_Game.py
@@ -0,0 +1,34 @@
+from random import shuffle
+
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input("Pick a number: 0, 1 or 2: ")
+    return int(guess)
+
+
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print('Correct!')
+    else:
+        print('Wrong guess!!')
+        print(mylist)
+
+
+# Initial List
+mylist = [' ', 'O', ' ']
+
+# Shuffle List
+mixedup_list = shuffle_list(mylist)
+
+# User Guess
+guess = player_guess()
+
+# Check guess
+check_guess(mixedup_list, guess)
